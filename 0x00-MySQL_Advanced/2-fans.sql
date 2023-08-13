77d5a85d6a22d725d57226ab2c9d2d18865f63e1
Alvin <alvinvaati@gmail.com>
2023-08-13 15:59:34 +0300

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,14 +1,11 @@
 -- Task: Rank country origins of bands by the number of non-unique fans
 -- Query to rank country origins by the number of non-unique fans
 
-SELECT
-    b.origin AS origin,
-    COUNT(DISTINCT f.fan_id) AS nb_fans
+SELECT origin,
+    COUNT(DISTINCT fans) AS nb_fans
 FROM
-    bands AS b
-LEFT JOIN
-    fans AS f ON b.band_id = f.band_id
+    metal_bands
 GROUP BY
-    b.origin
+    origin
 ORDER BY
     nb_fans DESC;
