36e17dca7d0f62053ec701ecd87c46f27cc50f34
Alvin <alvinvaati@gmail.com>
2023-08-13 16:41:56 +0300

    Optimize simple search

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,5 +1,3 @@
 -- Task: Create an index idx_name_first on the first letter of the name column
 
-DROP INDEX IF EXISTS idx_name_first ON names;
-
 CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
