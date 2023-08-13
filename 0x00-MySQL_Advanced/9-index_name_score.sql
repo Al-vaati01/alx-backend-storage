1bdd1ac3ae12b68f2f327a5aba4e291a5769b10e
Alvin <alvinvaati@gmail.com>
2023-08-13 16:50:33 +0300

    Optimize search and score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,3 @@
 -- Task: Create an index idx_name_first_score on the first letter of the name column and the score column
 
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
-CREATE INDEX idx_name_first_score ON names (SUBSTRING(name, 1, 1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
