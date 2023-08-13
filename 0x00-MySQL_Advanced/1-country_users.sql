339dfbe0107ef0b7b2fa9bfe7acae6cc01c4aea2
Alvin <alvinvaati@gmail.com>
2023-08-13 15:21:49 +0300

    create users table with country

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- This script creates a table called "users" with specified attributes
+-- If the table already exists, the script will not fail
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
