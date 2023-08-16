324e89dfd1456a268b20967f4fd5967f04cb4a7c
Alvin <alvinvaati@gmail.com>
2023-08-16 18:16:48 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+"""
+Using Redis
+"""
+
+import redis
+import uuid
+
+
+class Cache:
+    """Cache class
+    """
+
+    def __init__(self):
+        """Constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str) -> str:
+        """Store data in redis
+        """
+        key: str = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
